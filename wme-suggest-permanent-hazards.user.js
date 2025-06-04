f8510e278166727c8c03fd0fdc822f832c993492
Gavin Canon-Phratsachack <37220586+gncnpk@users.noreply.github.com>
2025-06-04 16:52:42 -0500

    Create wme-suggest-permanent-hazards.user.js

---
+++ wme-suggest-permanent-hazards.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         WME Suggest Permanent Hazards
+// @namespace    https://github.com/WazeDev/wme-suggest-permanent-hazards
+// @version      2025-06-04
+// @description  Allow lower map editors to add a map note for a permanent hazard.
+// @author       Gavin Canon-Phratsachack (https://github.com/gncnpk)
+// @match        https://beta.waze.com/*editor*
+// @match        https://www.waze.com/*editor*
+// @exclude      https://www.waze.com/*user/*editor/*
+// @exclude      https://www.waze.com/discuss/*
+// @license      MIT
+// @grant        none
+// @require https://cdn.jsdelivr.net/gh/WazeSpace/wme-sdk-plus@latest/wme-sdk-plus.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.SDK_INITIALIZED.then(initialize);
+    let sdk;
+    let sdkPlus;
+    let wmeSdk;
+    async function initialize() {
+  wmeSdk = await getWmeSdk({ scriptId: "wme-suggest-permanent-hazards", scriptName: "WME Suggest Permanent Hazards" }); // Assuming getWmeSdk is your method to get the native SDK.
+  sdkPlus = await initWmeSdkPlus(wmeSdk, {
+      hooks:["DataModel.MapComments"]
+  });
+  sdk = sdkPlus || wmeSdk;
+  sdk.Shortcuts.createShortcut({callback: function() {createSpeedBumpMapNote()}, description:"Create Speed Bump Note", shortcutId:"create-speed-bump-note",shortcutKeys:null})
+}
+ async function createSpeedBumpMapNote() {
+     const point = await sdk.Map.drawPoint();
+     await sdk.DataModel.MapComments.addMapComment({geometry: point, subject: "Speed Bump", body:"Add a speed bump permanent hazard here, once added, delete this map comment."})
+ }
+})();
